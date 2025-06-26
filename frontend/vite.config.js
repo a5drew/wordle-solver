018a32be27e438f31eb6d0788357ec5418c63810
Andrew <aandrew.p.rivera@gmail.com>
2025-06-25 22:36:44 -0700

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add this 'build' section to fix the 'import.meta' warning
+  build: {
+    target: 'esnext'
+  }
 })
